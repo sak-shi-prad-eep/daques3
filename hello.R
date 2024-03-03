cd4a6cfdd2b23289fd8e42d7c56926694a796777
sak-shi-prad-eep <sakshipradeep03@gmail.com>
2024-03-03 21:01:35 +0530

    Add hello.R with add_numbers function

---
+++ hello.R
@@ -0,0 +1,8 @@
+#' Function to add 2 numbers 
+#' @param number1 Numeric, the first number. 
+#' @param number2 Numeric, the second number. 
+#' @return Numeric, the sum of number1 and number2. 
+#' @export 
+  # Add two numbers and return the result 
+  return(number1 + number2) 
+} 
