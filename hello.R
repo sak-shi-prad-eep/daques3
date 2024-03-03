f56421d83240af2acfc9e14676c25a9786703cc0
sak-shi-prad-eep <sakshipradeep03@gmail.com>
2024-03-03 21:11:16 +0530

    Add hello.R with my_function21BDS0108 function

--- hello.R
+++ hello.R
@@ -1,8 +1,7 @@
-#' Function to add 2 numbers 
-#' @param number1 Numeric, the first number. 
-#' @param number2 Numeric, the second number. 
-#' @return Numeric, the sum of number1 and number2. 
-#' @export 
-  # Add two numbers and return the result 
-  return(number1 + number2) 
-} 
+'# Function to add 2 numbers' 
+'# @param a Numeric, the first number.' 
+'# @param b Numeric, the second number.' 
+'# @return Numeric, the sum of a and b.' 
+'# @export' 
+'  return(a + b)' 
+'}' 
